dfb5648de6b6bde1dded51a0de7ff1a3e7d00bad
Brandon R. Stoner <monokrome@monokro.me>
2010-12-20 01:49:35 -0700

    Added a shortcut for getting the proper destination for replying to received messages.

--- jinzou/util/shortcuts.py
+++ jinzou/util/shortcuts.py
@@ -3,3 +3,11 @@ def get_command_string(command):
 
     return settings['core']['command_format'].format(command)
 
+def get_reply_destination(client, user, channel):
+    if channel == client.nickname:
+        source = user.split('!', 1)[0]
+    else:
+        source = channel
+
+    return source
+
